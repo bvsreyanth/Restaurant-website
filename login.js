4d77fdcb6559f9fc088f7834de6809dc8816b61c
B Venkata Sreyanth <96385391+bvsreyanth@users.noreply.github.com>
2023-04-27 14:24:01 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,20 +1,20 @@
 function addtext()
         {
            var fname = document.main-form.first_name.value;
-        //    var lname = document.myform.last_name.value;
-        //    var email = document.myform.email.value;
+           var lname = document.main-form.last_name.value;
+            var email = document.main-form.email.value;
 
            document.writeln("Thank you! You have just entered the following:");
            document.writeln("<pre>");
            document.writeln("First Name    : " + fname);
-        //    document.writeln("Last Name     : " + lname);
-        //    document.writeln("Email Address : " + email);
+           document.writeln("Last Name     : " + lname);
+           document.writeln("Email Address : " + email);
         }
 
         function submitClick( ) //function shows message after submitting information
 
         {
       
-        alert("Thank you for your time! Your details have been submitted!");
+        alert("Thank you for your time! Your Table has been Booked!");
       
         }
