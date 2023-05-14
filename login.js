b4029e00312d34114548ab1a812c91b9a39b1c23
B Venkata Sreyanth <96385391+bvsreyanth@users.noreply.github.com>
2023-05-14 21:44:25 +0530

    login.js

--- login.js
+++ login.js
@@ -1,14 +1,18 @@
 function addtext()
         {
-           var fname = document.main-form.first_name.value;
-           var lname = document.main-form.last_name.value;
-            var email = document.main-form.email.value;
+           var name = document.main-form.name.value;
+           var email = document.main-form.email.value;
+           var time = document.main-form.time.value;
+           var date = document.main-form.date.value;
+           var phone = document.main-form.phone.value;
 
            document.writeln("Thank you! You have just entered the following:");
            document.writeln("<pre>");
-           document.writeln("First Name    : " + fname);
-           document.writeln("Last Name     : " + lname);
+           document.writeln(" Full Name    : " + name);
            document.writeln("Email Address : " + email);
+           document.writeln("Time : " + time);
+           document.writeln("Date : " + date);
+           document.writeln("Phone Number : " + phone);
         }
 
         function submitClick( ) //function shows message after submitting information
